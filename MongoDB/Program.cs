731c64c49e4dca9d04b95148a2e464c0f7cb698a
13935 <hl13935@ly.com>
2016-06-03 13:58:35 +0800

    添加mongodb

---
+++ MongoDB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoDbHelper<string> mongoHelper = new MongoDbHelper<string>();
+            mongoHelper.Insert("testName", "name");
+
+            Console.ReadLine();
+        }
+    }
+}
